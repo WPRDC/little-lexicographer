d69c32e316ecb8a45655d1416c5272899d8e113a
drw <drw@users.noreply.github.com>
2021-05-12 21:44:41 -0400

    Interpret integer-like fields with names like *_id as strings

--- lil_lex.py
+++ lil_lex.py
@@ -105,6 +105,10 @@ def choose_type(options, values, fieldname):
         if selection == 'int':
             return 'text'
 
+    if re.search('_id$', fieldname.lower()) is not None: # If it's an ID interpreted as an integer,
+        if selection == 'int':                           # force it to be a string.
+            return 'text'
+
     return selection
 
 base_schema_type = {'text': 'String',
